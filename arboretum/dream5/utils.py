15669a0a396168c1aadd54d47b6b040b385ae2cc
Thomas Moerman <thomas.moerman@gmail.com>
2017-10-17 21:44:50 +0200

    added dream5 graph factory

--- arboretum/dream5/utils.py
+++ arboretum/dream5/utils.py
@@ -2,7 +2,7 @@
 Utility functions for reading DREAM5 data.
 """
 
-import numpy as np
+from arboretum.core import *
 
 
 def load_expression_matrix(path, delimiter='\t'):
@@ -39,3 +39,22 @@ def load_tf_names(path, gene_names):
         tfs_in_file = [line.strip() for line in file.readlines()]
 
     return [tf for tf in tfs_in_file if tf in gene_names]
+
+
+def create_dream5_graph(ex_path, tf_path,
+                        regressor_type, regressor_kwargs,
+                        seed=DEMON_SEED):
+    """
+    :param ex_path: expression matrix file.
+    :param tf_path: transcription factor list.
+    :param regressor_type: one of ['RF', 'ET', 'GBM', 'XGB']. Case insensitive.
+    :param regressor_kwargs: dict of key-value pairs that configures the regressor.
+    :param seed: (optional) (optional) random seed for the regressors.
+    :return: a Dask computation graph.
+    """
+
+    expression_matrix = load_expression_matrix(ex_path)
+    gene_names = load_gene_names(ex_path)
+    tf_names = load_tf_names(tf_path, gene_names)
+
+    return create_graph(expression_matrix, gene_names, tf_names, regressor_type, regressor_kwargs, seed=seed)
