422596368bbcda8181625cf29a2dfca70715dd1c
Thomas Moerman <thomas.moerman@gmail.com>
2017-11-11 20:23:16 +0100

    added distributed dependency

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Arboretum',
+    name='arboretum',
     long_description=open('README.md').read(),
     version='1.0.0',
     license='LICENSE.txt',
     author='Thomas Moerman',
     author_email='thomas.moerman@esat.kuleuven.be',
     packages=find_packages(),
-    install_requires=['scikit-learn', 'numpy', 'pandas', 'dask'],
+    install_requires=['scikit-learn', 'numpy', 'pandas', 'dask', 'distributed'],
     platforms=['any'],
     keywords=['gene', 'regulatory', 'network', 'inference', 'regression', 'ensemble', 'scalable']
 )
