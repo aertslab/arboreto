14b44f5ef04028bfa519f98d7684109245887d0e
Thomas Moerman <thomas.moerman@gmail.com>
2017-11-11 20:26:58 +0100

    added scipy to install_requires

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Thomas Moerman',
     author_email='thomas.moerman@esat.kuleuven.be',
     packages=find_packages(),
-    install_requires=['scikit-learn', 'numpy', 'pandas', 'dask', 'distributed'],
+    install_requires=['scikit-learn', 'numpy', 'scipy', 'pandas', 'dask', 'distributed'],
     platforms=['any'],
     keywords=['gene', 'regulatory', 'network', 'inference', 'regression', 'ensemble', 'scalable']
 )
