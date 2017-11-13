2ca0904e327404263bb3afd0765630f21d96444f
Thomas Moerman <thomas.moerman@gmail.com>
2017-11-13 16:19:03 +0100

    towards PyPI package distribution

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='arboretum',
+    packages=['arboretum'],
     long_description=open('README.md').read(),
-    version='1.0.0',
+    url='https://github.com/tmoerman/arboretum',
+    download_url='https://github.com/tmoerman/arboretum/archive/0.1.tar.gz',
+    version='0.1',
     license='LICENSE.txt',
     author='Thomas Moerman',
-    author_email='thomas.moerman@esat.kuleuven.be',
-    packages=find_packages(),
+    author_email='thomas_DOT_moerman_AT_esat_DOT_kuleuven_DOT_be',
     install_requires=['scikit-learn', 'numpy', 'scipy', 'pandas', 'dask', 'distributed'],
     platforms=['any'],
     keywords=['gene', 'regulatory', 'network', 'inference', 'regression', 'ensemble', 'scalable']
