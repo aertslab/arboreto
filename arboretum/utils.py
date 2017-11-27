73f54ff05a4dc047684202f5ccac5c5034540ed2
Thomas Moerman <thomas.moerman@gmail.com>
2017-11-27 20:28:21 +0100

    remove dream specific functions

--- arboretum/utils.py
+++ arboretum/utils.py
@@ -2,31 +2,6 @@
 Utility functions for reading DREAM5 data.
 """
 
-from arboretum.core import *
-
-
-def load_expression_matrix(path, delimiter='\t'):
-    """
-    :param path: the path of the dream challenge expression data file.
-    :param delimiter: the delimiter used in the file.
-    :return: a numpy matrix.
-    """
-
-    return np.genfromtxt(path, delimiter=delimiter, skip_header=1)
-
-
-def load_gene_names(path, delimiter='\t'):
-    """
-    :param path: the path of the dream challenge expression data file.
-    :param delimiter: the delimiter used in the file.
-    :return: a list of gene names.
-    """
-
-    with open(path) as file:
-        gene_names = [gene.strip() for gene in file.readline().split(delimiter)]
-
-    return gene_names
-
 
 def load_tf_names(path):
     """
@@ -38,22 +13,3 @@ def load_tf_names(path):
         tfs_in_file = [line.strip() for line in file.readlines()]
 
     return tfs_in_file
-
-
-def create_dream5_graph(ex_path, tf_path,
-                        regressor_type, regressor_kwargs,
-                        seed=DEMON_SEED):
-    """
-    :param ex_path: expression matrix file.
-    :param tf_path: transcription factor list.
-    :param regressor_type: one of ['RF', 'ET', 'GBM', 'XGB']. Case insensitive.
-    :param regressor_kwargs: dict of key-value pairs that configures the regressor.
-    :param seed: (optional) (optional) random seed for the regressors.
-    :return: a Dask computation graph.
-    """
-
-    expression_matrix = load_expression_matrix(ex_path)
-    gene_names = load_gene_names(ex_path)
-    tf_names = load_tf_names(tf_path)
-
-    return create_graph(expression_matrix, gene_names, tf_names, regressor_type, regressor_kwargs, seed=seed)
