e91efba5976d9c21337a362be99e7278e654ddae
Thomas Moerman <thomas.moerman@gmail.com>
2017-12-12 10:41:26 +0100

    arboretum script with command line interface

---
+++ scripts/run_dream5_net1_genie3.sh
@@ -0,0 +1,5 @@
+python run_arboretum.py --genie3 \
+-i ../resources/dream5/net1/net1_expression_data.tsv \
+-tf ../resources/dream5/net1/net1_transcription_factors.tsv \
+-o net1_output.tsv \
+> net1_run_log.txt
---
+++ scripts/run_dream5_net1_grnboost2.sh
@@ -0,0 +1,5 @@
+python run_arboretum.py --grnboost2 \
+-i ../resources/dream5/net1/net1_expression_data.tsv \
+-tf ../resources/dream5/net1/net1_transcription_factors.tsv \
+-o net1_output.tsv \
+> net1_run_log.txt
