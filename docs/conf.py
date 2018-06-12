0b7ea60aa5229b365f37d8bf745d4ebd0e79c2f1
Thomas Moerman <thomas.moerman@gmail.com>
2018-06-12 15:58:10 +0200

    Update conf.py
    sphinx theme, cfr. code highlighting problem

--- docs/conf.py
+++ docs/conf.py
@@ -84,7 +84,7 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 RTD_NEW_THEME = True
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme>=0.3.1'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
