b92303b10bd7cf4a0f62e488426d81064aae668c
Chris Flerin <ccflerin@gmail.com>
2021-02-08 16:06:39 +0100

    Update setup.py - Move package requirements to requirement.txt - Update version, urls

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 from setuptools import setup, find_packages
 
+def read_requirements(fname):
+    with open(fname, 'r', encoding='utf-8') as file:
+        return [line.rstrip() for line in file]
+
 setup(
     name='arboreto',
     packages=find_packages(),
     description='Scalable gene regulatory network inference using tree-based ensemble regressors',
     long_description=open('README.rst').read(),
-    url='https://github.com/tmoerman/arboreto',
-    download_url='https://github.com/tmoerman/arboreto/archive/0.1.tar.gz',
-    version='0.1.5',
+    url='https://github.com/aertslab/arboreto',
+    version='0.1.6',
     license='BSD 3-Clause License',
     author='Thomas Moerman',
     author_email='thomas.moerman@gmail.com',
-    install_requires=['scikit-learn', 'numpy', 'scipy', 'pandas', 'dask', 'distributed'],
+    install_requires=read_requirements('requirements.txt'),
     platforms=['any'],
     keywords=['gene', 'regulatory', 'network', 'inference', 'regression', 'ensemble', 'scalable', 'dask']
 )
+
