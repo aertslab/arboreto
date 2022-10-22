41160d776b79f9294831f850048c81b9273cfa9a
removedporn <86824510+removedporn@users.noreply.github.com>
2022-10-22 08:18:00 +0800

    Delete scripts directory

--- scripts/run_dream5_net1_genie3.sh
+++
@@ -1,5 +0,0 @@
-python run_arboreto.py --genie3 \
--i ../resources/dream5/net1/net1_expression_data.tsv \
--tf ../resources/dream5/net1/net1_transcription_factors.tsv \
--o net1_output.tsv \
-> net1_run_log.txt
--- scripts/run_dream5_net1_grnboost2.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-python run_arboreto.py --grnboost2 \
--i ../resources/dream5/net1/net1_expression_data.tsv \
--tf ../resources/dream5/net1/net1_transcription_factors.tsv \
--o net1_output.tsv \
-> net1_run_log.txt
